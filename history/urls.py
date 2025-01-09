07b85baaeda756656308050aed4ba430ccdaf1b8
Kristupas <kr.jakubonis@gmail.com>
2025-01-09 14:23:17 +0000

    Selecting data and filling the inputs

--- history/urls.py
+++ history/urls.py
@@ -4,6 +4,6 @@ from history.views import index, view_cells
 
 urlpatterns = [
 	path('', index, name='history'),
-	path('history/<int:table_id>/cells/', view_cells, name='view_cells'),
+	path('<int:table_id>/cells/', view_cells, name='view_cells'),
 
 ]
