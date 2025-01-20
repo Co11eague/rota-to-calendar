8f867ef468980fdb4977367b81863a903b5e7ab7
Kristupas <kr.jakubonis@gmail.com>
2025-01-20 13:50:00 +0000

    Signup style update

--- accountSettings/models.py
+++ accountSettings/models.py
@@ -24,5 +24,5 @@ class UserSettings(models.Model):
 @receiver(post_save, sender=User)
 def create_or_update_user_profile(sender, instance, created, **kwargs):
     if created:
-        UserProfile.objects.create(user=instance)
+        UserSettings.objects.create(user=instance)
     instance.profile.save()
--- signup/forms.py
+++ signup/forms.py
@@ -8,8 +8,8 @@ class SignupForm(UserCreationForm):
 		max_length=200,
 		help_text='Required',
 		widget=forms.EmailInput(attrs={
-			'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500',
-			'placeholder': 'Enter your email'
+			'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+			'placeholder': 'guest@email.com'
 		})
 	)
 
@@ -18,15 +18,15 @@ class SignupForm(UserCreationForm):
 		fields = ['username', 'email', 'password1', 'password2']
 		widgets = {
 			'username': forms.TextInput(attrs={
-				'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500',
-				'placeholder': 'Enter your username'
+				'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+				'placeholder': 'username'
 			}),
 			'password1': forms.PasswordInput(attrs={
-				'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500',
-				'placeholder': 'Enter your password'
+				'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+				'placeholder': '••••••••'
 			}),
 			'password2': forms.PasswordInput(attrs={
-				'class': 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500',
-				'placeholder': 'Re-enter your password'
+				'class': 'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-4 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+				'placeholder': '••••••••'
 			}),
 		}
