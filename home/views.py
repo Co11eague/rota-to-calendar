43ca8d3f0ce6306c3cd2df221bec1627a7b7e6bf
Kristupas <kr.jakubonis@gmail.com>
2025-01-29 13:44:51 +0000

    Home UI changes

--- home/views.py
+++ home/views.py
@@ -1,9 +1,18 @@
 # views.py
 from django.shortcuts import render
 
+from accountProfile.models import UserProfile
+from accountSettings.models import UserSettings
 from .models import Slider
 
 
 def index(request):
 	sliders = Slider.objects.all()  # Fetch all slider images
-	return render(request, 'home/index.html', {'sliders': sliders})
+	if request.user.is_authenticated:
+		profile_picture = UserProfile.objects.get(user=request.user).profile_picture
+		dark = UserSettings.objects.get(user=request.user).darkMode
+
+	else:
+		profile_picture = None
+		dark = False
+	return render(request, 'home/index.html', {'sliders': sliders, 'profile_picture': profile_picture.url, 'dark': dark})
