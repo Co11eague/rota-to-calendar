352f0371f40b18cb480982b7ec713ee3fcf7f55d
Kristupas <kr.jakubonis@gmail.com>
2025-02-05 17:39:01 +0000

    Modal looks good need to work on backend a bit

--- accountSettings/forms.py
+++ accountSettings/forms.py
@@ -15,14 +15,14 @@ class SettingsForm(forms.ModelForm):
 
 		widgets = {
 			'darkMode': forms.CheckboxInput(attrs={
-				'class': 'sr-only peer',
+				'class': 'sr-only peer z-10',
 				'placeholder': 'Dark Mode'
 			}),
 			'ocrRecognition': forms.Select(attrs={
 				'class': 'text-sm text-gray-700 dark:text-gray-200 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-md p-1',
 			}),
 			'saveToCalendar': forms.CheckboxInput(attrs={
-				'class': 'sr-only peer',
+				'class': 'sr-only peer z-10',
 			})
 		}
 
--- history/static/history/js/index.js
+++ history/static/history/js/index.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     const option = document.getElementById("sorting")
     const searchField = document.getElementById("searchField")
+    const eventModal = document.getElementById('eventModal');
+    const closeModalBtn = document.getElementById("cancelEventBtn");
+    const overlay = document.getElementById('overlay');
 
 
     const urlParams = new URLSearchParams(window.location.search)
@@ -11,8 +14,61 @@ document.addEventListener('DOMContentLoaded', function () {
         option.value = sortValue;
     }
 
-    if (searchValue){
+    if (searchValue) {
         searchField.value = searchValue
     }
+
+    function openModal() {
+
+        // Get user's current scroll position
+        const scrollY = window.scrollY;
+        const viewportHeight = window.innerHeight;
+        const modalHeight = eventModal.clientHeight;
+
+        // Position modal in the center of the current viewport
+        eventModal.style.top = `${scrollY + (viewportHeight / 2) - (modalHeight / 2)}px`;
+        eventModal.style.left = "50%";
+        eventModal.style.transform = "translateX(-50%)";
+
+        eventModal.classList.remove('hidden');
+        document.body.style.pointerEvents = "none";
+        eventModal.style.pointerEvents = "auto";
+        overlay.classList.remove('hidden');
+
+    }
+
+    function closeModal() {
+        eventModal.classList.add("hidden");
+        document.body.style.pointerEvents = "auto";
+        overlay.classList.add('hidden');
+
+    }
+
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.addEventListener('click', openModal);
+    });
+
+    closeModalBtn.addEventListener("click", closeModal);
+
+    const populateInput = (buttonId, inputId) => {
+        const buttons = document.querySelectorAll(`#${buttonId}`);
+        buttons.forEach(button => {
+            button.addEventListener("click", () => {
+                const cell = button.closest('.cell');  // Find the .cell element containing this button
+                const ocrText = cell.getAttribute('data-text');  // Get the OCR text from the data-text attribute
+                document.querySelector(`#${inputId}`).value = ocrText;  // Populate the corresponding input field
+            });
+        });
+    };
+
+    // Call the function for each button/input pairing
+    populateInput("name", "name-input");
+    populateInput("start-time", "start-time-input");
+    populateInput("start-date", "start-date-input");
+    populateInput("location", "location-input");
+    populateInput("end-time", "end-time-input");
+    populateInput("end-date", "end-date-input");
+
 
 })
