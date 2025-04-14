db8af03b3d5db649228735c677c84ff1e3edc50c
Kristupas <kr.jakubonis@gmail.com>
2025-04-14 15:43:17 +0100

    Cleanup + documentation

--- documentation/views.py
+++ documentation/views.py
@@ -1,8 +1,20 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from accountProfile.models import UserProfile
+from accountSettings.models import UserSettings
+
 
 @login_required
 def index(request):
-	return render(request, 'documentation/index.html')
+	if request.user.is_authenticated:
+		user_settings = UserSettings.objects.get(user=request.user)
+		dark = user_settings.darkMode
+		user_profile = UserProfile.objects.get(user=request.user)
+	else:
+		dark = False
+		user_profile = None
+
+	return render(request, 'documentation/index.html', {'dark': dark,
+	                                                    'profile_picture': user_profile.profile_picture if user_profile and user_profile.profile_picture else None})
 # Create your views here.
